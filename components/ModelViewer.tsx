94564b02062b4ce5be21f91ca9770478d2124c4b
Kazukiworld <kazukistartupsforall@gmail.com>
2023-05-20 15:04:00 -1000

    feat: animate model position, light, camera zoom

--- components/ModelViewer.tsx
+++ components/ModelViewer.tsx
@@ -1,34 +1,36 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Canvas, useLoader, useThree} from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import React, { SetStateAction, useRef, useState } from 'react'
+import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
-import { OrbitControls, Html, Environment, SpotLight } from '@react-three/drei';
+import { OrbitControls, OrthographicCamera } from '@react-three/drei';
 import { Moon } from './Moon';
+import { useSpring, a } from 'react-spring';
+import { Object3D } from 'three';
 
 export default function ModelViewer() {
+    const lightTarget = useRef<Object3D | null>(null);
+    const [position, setPosition] = useState<number[] | null>(null);
+    const [lightPosition, setLightPosition] = useState<number[] | null>(null);
+    const [zoomLevel, setZoomLevel] = useState<number | null>(0.7);
 
-    const lightTarget: any = useRef();
-    
+    const updatePosition = () => {
+        setPosition([0, 0, 3]);
+        setLightPosition([-5, -5, -7]);
+        setZoomLevel(1.3);
+    };
+
     return (
-        <div className='h-full'>
+        <div className='h-full relative'>
+            <button className='w-24 h-24 bg-red z-20' onClick={updatePosition}>
+                Hello
+            </button>
             <Canvas>
                 <OrbitControls />
                 <Suspense fallback={null}>
-                    <Moon/>
-                    <directionalLight
-                        position={[18, -5, -5]}
-                        intensity={1}
-                        castShadow
-                        shadow-mapSize-width={1024}
-                        shadow-mapSize-height={1024}
-                        shadow-bias={-0.0001}
-                        target={lightTarget.current}
-                    />
-                    <ambientLight intensity={0.03} />
+                    <Moon position={position} lightPosition={lightPosition} zoom={zoomLevel}/>
+                    <ambientLight intensity={0.02} />
                     <object3D ref={lightTarget} position={[0, 0, 0]} />
-                    {/* <Environment preset="studio" /> */}
                 </Suspense>
             </Canvas>
         </div>
-    )
+    );
 }
--- components/Moon.jsx
+++ components/Moon.jsx
@@ -1,20 +1,36 @@
-import React, { useRef } from 'react'
-import { useGLTF, useAnimations} from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
+import React, { useRef, useState } from 'react'
+import { useGLTF, useAnimations } from '@react-three/drei'
+import { useThree, useFrame } from '@react-three/fiber'
+import { useSpring, a } from '@react-spring/three'
 
-export function Moon(props) {
+export function Moon({ position, lightPosition, zoom, ...props}) {
   const group = useRef();
+  const lightRef = useRef();
   const { nodes, materials, animations } = useGLTF('/moon/moon.gltf');
   const { actions } = useAnimations(animations, group);
+  const { size, viewport, camera } = useThree();
+  const defaultPosition = position || [viewport.width / 2.6, viewport.height / 2.6, 0];
+  const defaultLightPosition = lightPosition ||[-5, -3, 2];
 
-    useFrame(({ clock }) => {
-      if (group.current) {
-        group.current.rotation.y += 0.001;
-      }
-    });
-    
+  const { posAnim, lightPosAnim, zoomAnim } = useSpring({
+    posAnim: defaultPosition,
+    lightPosAnim: defaultLightPosition,
+    zoomAnim: zoom,
+    config: { mass: 1, tension: 280, friction: 120 }
+  });
+
+  useFrame(({ clock }) => {
+    if (group.current) {
+      group.current.rotation.y += 0.0004;
+    }
+    camera.zoom = zoomAnim.get();
+    camera.updateProjectionMatrix();
+  });
+
+
   return (
-    <group ref={group} {...props} dispose={null}>
+    <a.group ref={group} {...props} dispose={null} position={posAnim}>
+      <a.directionalLight ref={lightRef} position={lightPosAnim} castShadow />
       <group name="Sketchfab_Scene">
         <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]}>
           <group name="b964401c34154430bb1a8d7a5fa66a66fbx" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
@@ -35,8 +51,51 @@ export function Moon(props) {
           </group>
         </group>
       </group>
-    </group>
+    </a.group>
   )
 }
 
 useGLTF.preload('/moon/moon.gltf')
+
+// import React, { useRef } from 'react'
+// import { useGLTF, useAnimations} from '@react-three/drei'
+// import { useFrame } from '@react-three/fiber'
+
+// export function Moon(props) {
+//   const group = useRef();
+//   const { nodes, materials, animations } = useGLTF('/moon/moon.gltf');
+//   const { actions } = useAnimations(animations, group);
+
+//     useFrame(({ clock }) => {
+//       if (group.current) {
+//         group.current.rotation.y += 0.001;
+//       }
+//     });
+    
+//   return (
+//     <group ref={group} {...props} dispose={null}>
+//       <group name="Sketchfab_Scene">
+//         <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]}>
+//           <group name="b964401c34154430bb1a8d7a5fa66a66fbx" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+//             <group name="Object_2">
+//               <group name="RootNode">
+//                 <group name="Sphere" rotation={[-1.26, 0.49, 0.96]} scale={100}>
+//                   <mesh name="Sphere_Material001_0" geometry={nodes.Sphere_Material001_0.geometry} material={materials['Material.001']} />
+//                   <mesh name="Sphere_Material001_0_1" geometry={nodes.Sphere_Material001_0_1.geometry} material={materials['Material.001']} />
+//                   <mesh name="Sphere_Material001_0_2" geometry={nodes.Sphere_Material001_0_2.geometry} material={materials['Material.001']} />
+//                   <mesh name="Sphere_Material001_0_3" geometry={nodes.Sphere_Material001_0_3.geometry} material={materials['Material.001']} />
+//                   <mesh name="Sphere_Material001_0_4" geometry={nodes.Sphere_Material001_0_4.geometry} material={materials['Material.001']} />
+//                 </group>
+//                 <group name="Camera" position={[528.6, 195.6, -369.36]} rotation={[3.12, -0.61, 2.84]} scale={100}>
+//                   <group name="Object_11" />
+//                 </group>
+//               </group>
+//             </group>
+//           </group>
+//         </group>
+//       </group>
+//     </group>
+//   )
+// }
+
+// useGLTF.preload('/moon/moon.gltf')
